5c3f929dc32e2fd0e2e09a83e1e14226f6f8b77e
BI6-Medic <bi6@medic.com.br>
2024-10-07 16:37:16 -0300

    Btn do faq adicionado

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ function ocult() {
 }
 
 ocult();
+
+const pacienteBtn = document.getElementById('paciente-btn');
+const medicoBtn = document.getElementById('medico-btn');
+const pacienteFaq = document.getElementById('paciente-faq');
+const medicoFaq = document.getElementById('medico-faq');
+
+function toggleFaq(role) {
+    if (role === 'paciente') {
+        pacienteFaq.style.display = 'block';
+        medicoFaq.style.display = 'none';
+        pacienteBtn.classList.add('active');
+        medicoBtn.classList.remove('active');
+    } else {
+        medicoFaq.style.display = 'block';
+        pacienteFaq.style.display = 'none';
+        medicoBtn.classList.add('active');
+        pacienteBtn.classList.remove('active');
+    }
+}
+
+pacienteBtn.addEventListener('click', () => toggleFaq('paciente'));
+medicoBtn.addEventListener('click', () => toggleFaq('medico'));
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
